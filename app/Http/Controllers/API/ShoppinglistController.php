3a2fccaaaa121182ed45e0db0c0f78a811328eeb
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-12-27 16:31:08 +0100

    first attempt at building api

---
+++ app/Http/Controllers/API/ShoppinglistController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShoppinglistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $shoppinglists = Shoppinglist::get()->toJson(JSON_PRETTY_PRINT);
+        return response()->json($shoppinglists, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if(Shoppinglist::where('id', $id)->exists())
+        {
+          $list = Shoppinglist::get('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+          return response()->json($list, 200);
+        } else {
+          return response()->json('not found!', 401);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
