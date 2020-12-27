cc3d72dc2840c98aa3c665b295d915be17249bbe
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-12-27 20:53:49 +0100

    dsd

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ShoppinglistController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +18,5 @@ use App\Http\Controllers\API\ShoppinglistController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('api')->group(function(){
-  Route::apiResource('shoppinglist', [ShoppinglistController::class]);
-})
+
+Route::apiResource('shoppinglist', ShoppinglistController::class);
