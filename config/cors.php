be78e109b8a6986c675c6f2404c3471b2a3ca5fb
MartijnvanderBruggen <Martijnvanderbruggen@live.nl>
2020-12-28 14:09:31 +0100

    trte

--- config/cors.php
+++ config/cors.php
@@ -19,16 +19,17 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://192.168.1.2:8080'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
     'supports_credentials' => false,
+
 
 ];
